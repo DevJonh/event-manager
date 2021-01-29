bc6330b3703333e3c2e30636de2ed98287bdd01f
Jonatas Oliveira <jhonnyoliveira.jhon@gmail.com>
2021-01-29 11:47:37 -0300

    Performed the filter by the search.

--- src/view/Home/index.js
+++ src/view/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import firebase from '../../config/firebase'
@@ -10,10 +10,9 @@ import './styles.css'
 
 const Home = () => {
   const [events, setEvents] = useState([])
+  const [search, setSearch] = useState('')
 
-  let listEvent = useMemo(() => {
-    return []
-  }, [])
+  let listEvent = []
 
   useEffect(() => {
     firebase
@@ -22,19 +21,31 @@ const Home = () => {
       .get()
       .then(async (response) => {
         await response.docs.forEach((doc) => {
-          listEvent.push({
-            id: doc.id,
-            ...doc.data()
-          })
+          if (doc.data().title.indexOf(search) >= 0) {
+            listEvent.push({
+              id: doc.id,
+              ...doc.data()
+            })
+          }
         })
 
         setEvents(listEvent)
       })
-  }, [listEvent])
+  })
 
   return (
     <>
       <NavBar />
+      <div className="w-100 row my-5 px-3 mx-auto">
+        <h3 className="text-center p-3">Eventos Publicados</h3>
+        <input
+          type="text"
+          className="form-control text-center"
+          placeholder="Pesquisar Eventos."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
       <div className="w-100 row p-3">
         {events.map((event) => (
           <CardEvent
