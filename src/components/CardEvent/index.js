2097d989944bab55b5f3fd05fc68767b0fadac1a
Jonatas Oliveira <jhonnyoliveira.jhon@gmail.com>
2021-01-29 11:08:52 -0300

    display of registered events

---
+++ src/components/CardEvent/index.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import firebase from '../../config/firebase'
+
+import { MdRemoveRedEye } from 'react-icons/md'
+
+import './styles.css'
+
+const CardEvent = ({ title, id, img, details, views }) => {
+  const [urlImage, setUrlImage] = useState()
+
+  useEffect(() => {
+    firebase
+      .storage()
+      .ref(`imagens/${img}`)
+      .getDownloadURL()
+      .then(async (url) => await setUrlImage(url))
+  }, [img])
+
+  return (
+    <div className="col-md-3 col-sm-12">
+      <img
+        src={urlImage}
+        className="card-img-top img-cartao"
+        alt="Imagem do Evento"
+      />
+      <div className="card-body">
+        <h5>{title}</h5>
+        <p className="card-text text-justify">{details}</p>
+
+        <div className="row rodape-card d-flex align-items-center justify-content-between">
+          <div className="col-6">
+            <Link className="btn btn-sm btn-detalhes" to={`/event/${id}`}>
+              + detalhes
+            </Link>
+          </div>
+
+          <div className="col-6 d-flex justify-content-end align-items-center">
+            <MdRemoveRedEye />
+            <span className="ms-1">{views}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CardEvent
--- src/view/Home/index.js
+++ src/view/Home/index.js
@@ -1,12 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import { Link } from 'react-router-dom'
 
+import firebase from '../../config/firebase'
+
+import CardEvent from '../../components/CardEvent'
 import NavBar from '../../components/NavBar'
 
 import './styles.css'
 
 const Home = () => {
-  return <NavBar />
+  const [events, setEvents] = useState([])
+
+  let listEvent = useMemo(() => {
+    return []
+  }, [])
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection('events')
+      .get()
+      .then(async (response) => {
+        await response.docs.forEach((doc) => {
+          listEvent.push({
+            id: doc.id,
+            ...doc.data()
+          })
+        })
+
+        setEvents(listEvent)
+      })
+  }, [listEvent])
+
+  return (
+    <>
+      <NavBar />
+      <div className="w-100 row p-3">
+        {events.map((event) => (
+          <CardEvent
+            key={event.id}
+            id={event.id}
+            img={event.image}
+            title={event.title}
+            details={event.description}
+            views={event.views}
+          />
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default Home
